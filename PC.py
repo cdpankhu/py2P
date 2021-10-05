42133ea7ae1070a35fb5a4a691cd66face98139f
cdpankhu <cdpankhu@uwaterloo.ca>
2021-10-05 14:38:22 -0400

    Changes to tradefunction and PC for function of first iteration internal

--- PC.py
+++ PC.py
@@ -1,9 +1,12 @@
 # PC.py
 import gurobipy
-import pandas
+from pandas import DataFrame
+from numpy import zeros
 from py2P.NetworkDataType import Bus, Line, Generator
 from py2P.P2PTradeType import Trade, Agent
 from py2P.NetworkLoad import networkload
+from py2P.TradeFunction import generatetrade, selecttrade
+from math import ceil
 
 
 testsystem = "AP15busDN"
@@ -11,67 +14,202 @@ testsystem = "AP15busDN"
 
 def pc(testsystem):
     buses, lines, generators, datamat = networkload(testsystem)
-    lineset = list(range(len(lines)))
-    busset = list(range(len(buses)))
-    genset = list(range(len(generators)))
-    buses[0].Vmax = 1
-    buses[0].Vmin = 1
-    windset = []
+    buses[1].Vmax = 1
+    buses[1].Vmin = 1
+    windset = {}
+    windex = 1
 
-    for g in genset:
+    for g in generators:
         if generators[g].gtype == "Wind":
-            windset.append(generators[g].gindex)
+            windset[windex] = generators[g].gindex
+            windex += 1
 
     root = -1
-    for g in genset:
+    for g in generators:
         if generators[g].gtype == "Root":
             root = g
 
     if root == -1:
         print("No generator at root node")
-        generators[0].gtype = "Root"
-        root = 0
+        generators[1].gtype = "Root"
+        root = 1
 
-    gensetU = root
-    gensetP = genset
-    del gensetP[root]
+    gensetU = generators[root]
+    gensetP = generators
+    gensetP.pop(root)
 
     # Defining the set of generator buses and demand buses
     B_g = []
-    for g in genset:
+    for g in generators:
         if not (generators[g].location in B_g):
             B_g.append(generators[g].location)
+            # For generation bus, set demand to 0
+            # This needs to be changed with updated model for storage, DR, etc.
             buses[generators[g].location].Pd = 0
-    B_d = [i for i in busset if i not in B_g]
+    B_d = [i for i in buses if i not in B_g]
 
     # Defining matrix of generators at each bus
-    B_gn = []
-    for b in busset:
-        B_gn.append([])
-    for g in genset:
-        B_gn[generators[g].location-1].append(g)
+    B_gn = {}
+    for b in buses:
+        B_gn[b] = []
+    for g in generators:
+        B_gn[generators[g].location].append(g)
 
     # Share of demand from utility versus peers
-    partlevel = 0.0
-    gam = partlevel*[1 for i in range(len(busset))]
+    partlevel = 1.0
+    gam = {}
+    for b in buses:
+        gam[b] = partlevel
 
     # Scale of each trade in MWh
     trade_scale = 1e-3
 
     # Defining trading agents at non-root buses
-    agents = []
-    for b in busset:
+    # Current definition defines a single agent of type 1 at each demand bus.
+    # Current definition defines duplicate generator agents for each gen at bus
+    agents = {}
+    agentID = 1
+    for b in buses:
         if not B_gn[b]:
-            temp_agent = Agent(
-                b+1, b+1, 1, 0, 0, [], gam[b]*buses[b].Pd, gam[b].buses[b].Qd)
-            agents.append(temp_agent)
+            agents[agentID] = Agent(
+                b, b, 1, 0, 0, [0, 0, 0], gam[b]*buses[b].Pd, gam[b]*buses[b].Qd)
+            agentID += 1
+            # agents[b].append(Agent(
+            #     b, b, 1, 0, 0, [], gam[b]*buses[b].Pd, gam[b]*buses[b].Qd))
         else:
             for g in B_gn[b]:
                 if generators[g].gtype != "Root":
-                    temp_agent = Agent(
-                        b+1, b+1, 1, generators[g].Pmin, generators[g].Pmax,
+                    agents[agentID] = Agent(
+                        b, b, 1, generators[g].Pmin, generators[g].Pmax,
                         generators[g].cost, gam[b]*buses[b].Pd,
                         gam[b]*buses[b].Qd)
-                    agents.append(temp_agent)
+                    agentID += 1
+                    # agents[b].append(Agent(
+                    #     b, b, 1, generators[g].Pmin, generators[g].Pmax,
+                    #    generators[g].cost, gam[b]*buses[b].Pd,
+                    #    gam[b]*buses[b].Qd))
 
-    # Defining trades
+    # Defining trades and result matrices
+    trades = generatetrade(agents, trade_scale)
+    trades_old = generatetrade(agents, trade_scale)
+
+    trades_mat = {}
+    trades_mat_buy = {}
+    trades_mat_sell = {}
+    for i in agents:
+        for j in agents:
+            trades_mat[i, j] = []
+            trades_mat_buy[i, j] = []
+            trades_mat_sell[i, j] = []
+    trade_selected_mat = {}
+    trade_selected_mat_buy = {}
+    trade_selected_mat_sell = {}
+    for i in buses:
+        for j in buses:
+            trade_selected_mat[i, j] = []
+            trade_selected_mat_buy[i, j] = []
+            trade_selected_mat_sell[i, j] = []
+    for w in trades:
+        trades_mat[trades[w].As, trades[w].Ab].append(trades[w].tradeID)
+
+    trades_selected = []
+    trades_rest = []
+    trades_selected_old = []
+    trades_rest_old = []
+    dispatch = {}
+    GenCon = {}
+    Generation = {}
+    Consumption = {}
+    GenCon_old = {}
+    dispatch_old = {}
+    for i in buses:
+        dispatch[i] = 0
+        GenCon[i] = 0
+        Generation[i] = 0
+        Consumption[i] = 0
+        GenCon_old[i] = 0
+        dispatch_old[i] = 0
+    status = 0
+    DLMP = {}
+    DLMP_stack = []
+    dispatch_stack = []
+    DLMPInfo = DataFrame()
+    NodeInfo = DataFrame()
+    LineInfo = DataFrame()
+    dispatch_peerG = {}
+    for i in generators:
+        dispatch_peerG[i] = 0
+    param_delta = 1.0
+
+    # need to add timer code for performance assessment
+    while True:
+        accepted = {}
+        rejected = {}
+        pg = {}
+        Revenue_Sell = {}
+        Cost_Buy = {}
+        Cost_Network = {}
+        Cost_DG = {}
+        trades_old = trades
+        trades_selected_old = trades_selected
+        for w in trades:
+            trades[w].Pes = 0
+            trades[w].Peb = 0
+        while True:
+            accepted = {}
+            rejected = {}
+            pg = {}
+
+            gap = 1e-3
+            for agentID in agents:
+                temp_accepted, temp_rejected, temp_pg, Revenue_Sell, Cost_Buy, \
+                    Cost_Network, Cost_DG = \
+                    selecttrade(trades, agents, agentID, gap, trade_scale)
+                accepted[agentID] = temp_accepted
+                rejected[agentID] = temp_rejected
+                pg[agentID] = temp_pg
+
+            # price delta = $5/MWh = $0.005/kWh
+            deltaP = 5*trade_scale
+            for w in trades:
+                if (w in accepted[trades[w].Ab]) \
+                        and not (w in accepted[trades[w].As]):
+                    if trades[w].Peb > trades[w].Pes:
+                        trades[w].Pes += deltaP
+                    else:
+                        trades[w].Peb += deltaP
+            diff = -1
+            if not trades:
+                diff = 0
+            else:
+                diff = sum((w in accepted[trades[w].Ab]) and not (
+                    w in accepted[trades[w].As]) for w in trades)
+
+            if diff == 0:
+                break
+
+        trades_selected = []
+        trades_rest = []
+        for w in trades:
+            if (w in accepted[trades[w].Ab]) and (w in accepted[trades[w].As]):
+                trades_selected.append(w)
+            else:
+                trades_rest.append(w)
+
+        for i in agents:
+            dispatch[i] = ceil(agents[i].Pd/trade_scale)  # MW to kW conversion
+
+        # Determine nodal operating points according to incremental trades
+        for w in trades_selected:
+            dispatch[agents[trades[w].Ab].location] -= 1 * trade_scale
+            dispatch[agents[trades[w].As].location] += 1 * trade_scale
+            GenCon[agents[trades[w].Ab].location] -= 1 * trade_scale
+            GenCon[agents[trades[w].As].location] += 1 * trade_scale
+            Consumption[agents[trades[w].Ab].location] -= 1 * trade_scale
+            Generation[agents[trades[w].As].location] += 1 * trade_scale
+
+        # Set generator dispatch values
+        for g in gensetP:
+            dispatch_peerG[g] = Generation[generators[g].location]
+
+        return dispatch, GenCon, Consumption, Generation
--- TradeFunction.py
+++ TradeFunction.py
@@ -5,18 +5,18 @@ from gurobipy import *
 
 
 def generatetrade(agents, trade_scale):
-    trades = []
+    trades = {}
     tradeID = 1
 
-    agentset = list(range(len(agents)))
-    for s in agentset:
-        for b in agentset:
+    for s in agents:
+        for b in agents:
             up = min(
                 floor(agents[s].pgMax/trade_scale),
                 ceil(agents[b].Pd/trade_scale))
-            for k in range(1, up):
+            # +1 since range is < max
+            for k in range(1, up+1):
                 w = Trade(tradeID, 1, 0.0, 0.0, s, b, 0)
-                trades.append(w)
+                trades[tradeID] = w
                 tradeID += 1
     return trades
 
@@ -24,14 +24,77 @@ def generatetrade(agents, trade_scale):
 def selecttrade(W, A, agentID, gap, trade_scale):
     # Trade Set W
     m = Model("selecttrade")
-
+    m.Params.OutputFlag = 0
     # Creat variables
-    u = {}
-    for w in range(len(W)):
-        u[w] = m.addVar(vtype=GRB.BINARY, name="u")
+    # Note, need to add constraint/variable for utility of marginal demand
+    pg = m.addVar(name="pg", lb=A[agentID].pgMin, ub=A[agentID].pgMax)
+    revenuesell = m.addVar(name="revenuesell")
+    costbuy = m.addVar(name="costbuy")
+    costnetwork = m.addVar(name="costnetwork")
+    costdg = m.addVar(name="costdg")
+    u = m.addVars(len(W), vtype=GRB.BINARY, name="u")
+
+    m.update()
 
     # Set objective functions
+    obj = revenuesell
+    obj -= costbuy
+    obj -= costnetwork
+    obj -= costdg
+    m.setObjective(obj, GRB.MAXIMIZE)
 
     # Create constraints
+    # Define revenue from sales to peers
+    m.addConstr(
+        revenuesell
+        == sum(W[i].Pes * u[i-1] for i in W if W[i].As == agentID),
+        "c0")
+    # Define cost of purchasing from peers
+    m.addConstr(
+        costbuy == sum(W[i].Peb * u[i-1] for i in W if W[i].Ab == agentID),
+        "c1")
+    # Define cost of network on transactions
+    m.addConstr(
+        costnetwork == trade_scale * (
+            sum(W[i].costNw * u[i-1] for i in W if W[i].Ab == agentID)
+            + sum(W[i].costNw * u[i-1] for i in W if W[i].As == agentID)),
+        "c2")
+    # Define cost of DG generation operation
+    m.addConstr(
+        costdg == (
+            A[agentID].costFn[0]*pg*pg + A[agentID].costFn[1]*pg
+            + A[agentID].costFn[2]
+            ),
+        "c3")
+    # Require that power from Dg is equal to the sum of selling trades
+    # Need to make sure trading to oneself is okay!
+    m.addConstr(
+        pg/trade_scale == sum(u[i-1] for i in W if W[i].As == agentID),
+        "c4")
+    # Requires that the peer demand is equal to the sum of purchased trades
+    # Currently Pd is fixed, no marginal value of additional demand. FIX!!!
+    m.addConstr(
+        floor(A[agentID].Pd/trade_scale)
+        == sum(u[i-1] for i in W if W[i].Ab == agentID),
+        "c5")
 
     # Run optimization
+    for i in W:
+        if not (W[i].As == agentID or W[i].Ab == agentID):
+            m.addConstr(u[i-1] == 0)
+
+    m.optimize()
+    v = m.getVars()
+    w = {}
+    tradeID = 1
+    for i in range(5, len(v)):
+        w[tradeID] = v[i].x
+        tradeID += 1
+    accepted = [i for i in w if w[i] == 1]
+    rejected = [i for i in w if w[i] == 0]
+    pg = v[0].x
+    revenuesell = v[1].x
+    costbuy = v[2].x
+    costnetwork = v[3].x
+    costdg = v[4].x
+    return accepted, rejected, pg, revenuesell, costbuy, costnetwork, costdg
